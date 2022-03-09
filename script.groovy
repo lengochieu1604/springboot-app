2d0f8775571730a742bdcd7f4a2694f54819fd8c
lengochieu1604 <lengochieu1604@gmail.com>
2022-03-09 21:50:16 +0700

    update script.groovy file

--- script.groovy
+++ script.groovy
@@ -17,6 +17,7 @@ def createDockerimage(){
 }
 def runContainer(){ 
     sh 'docker run -d thetips4you/springboot' 
-    echo "Running an image inside of a container"       
+    echo "Running an image inside of a container."       
 }
 return this
+
