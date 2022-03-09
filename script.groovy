56b1a6798403b677b5f7faadf14ea66503e78cc8
Lê Ngọc Hiếu <98753976+lengochieu1604@users.noreply.github.com>
2022-03-09 05:51:26 -0800

    add groovy file

---
+++ script.groovy
@@ -0,0 +1,21 @@
+def gitClone(){
+    git 'https://github.com/shazforiot/HelloWorld-Springboot-App.git' 
+    echo "Cloning completed"
+}
+def mavenTest(){
+    sh 'mvn test'
+    echo "Testing completed"
+}
+def mavenBuild(){
+    sh 'mvn package'
+    echo "Building completed"
+}
+def createDockerimage(){
+    sh 'docker build -t thetips4you/springboot:latest .'
+    echo "Building Dockerimage completed"
+            
+}
+def runContainer(){ 
+    sh 'docker run -d thetips4you/springboot' 
+    echo "Running an image inside of a container"       
+}
